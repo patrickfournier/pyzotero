7d91c262fc4eb7217050341d96e6984f8942efb8
Patrick Fournier <patrick@whiskyechobravo.com>
2014-10-17 12:50:45 -0400

    delete_collection now accepts a list of collections to delete.

--- pyzotero/zotero.py
+++ pyzotero/zotero.py
@@ -1140,20 +1140,34 @@ class Zotero(object):
     def delete_collection(self, payload):
         """
         Delete a Collection from a Zotero library
-        Accepts a single argument: a dict containing item data
+        Accepts a single argument:
+            a dict containing item data
+            OR a list of dicts containing item data
         """
-        modified = payload['version']
-        ident = payload['key']
+        params = None
+        if isinstance(payload, list):
+            params = {'collectionKey': ','.join([p['key'] for p in payload])}
+            modified = payload[0]['version']
+            url = self.endpoint + \
+            '/{t}/{u}/collections'.format(
+                t=self.library_type,
+                u=self.library_id)
+        else:
+            ident = payload['key']
+            modified = payload['version']
+            url = self.endpoint + \
+            '/{t}/{u}/collections/{c}'.format(
+                t=self.library_type,
+                u=self.library_id,
+                c=ident)
         headers = dict({
             'If-Unmodified-Since-Version': modified}.items()
             + self.default_headers().items())
         req = requests.delete(
-            url=self.endpoint
-            + '/{t}/{u}/collections/{c}'.format(
-                t=self.library_type,
-                u=self.library_id,
-                c=ident),
+            url=url,
+            params=params,
             headers=headers)
+
         try:
             req.raise_for_status()
         except requests.exceptions.HTTPError:
